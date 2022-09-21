7e7dcf154c4a71b473cbc093b8b926d437fbcb11
RuchaRK <ruchakathar@gmail.com>
2022-09-21 19:08:12 +0530

    setting up the quiz app

---
+++ quiz.js
@@ -0,0 +1,24 @@
+var quizInputs = document.querySelector('.quizForm')
+var submitButton = document.querySelector("#submitAns")
+var outputMsg = document.querySelector("#output")
+
+var correctAns = ["90","right"]
+
+function checkScores()
+{
+    var score=0
+    var index=0
+    const data = new FormData(quizInputs);
+    for (const entry of data.values() )
+    {
+        if(entry === correctAns[index])
+        {
+            score=score+1
+        }
+        index=index+1
+
+    }
+    outputMsg.innerText = "Your score is " + score;
+}
+
+submitButton.addEventListener('click',checkScores)
